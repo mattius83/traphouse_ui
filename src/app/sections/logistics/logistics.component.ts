1675e1b32e646d89be11ca44fab6f2836f25033d
Matthew Toll <matttoll@masterpeaceltd.com>
2019-03-16 11:14:05 -0400

    tweaks to ensure angular material working and that jointJS packages do not interfere

--- src/app/sections/logistics/logistics.component.ts
+++ src/app/sections/logistics/logistics.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:JQueryStatic;
+import * as _ from 'lodash';
+import * as backbone from 'backbone';
+import * as joint from 'jointjs';
 
 @Component({
   selector: 'app-logistics',
@@ -10,6 +14,31 @@ export class LogisticsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let graph = new joint.dia.Graph;
+
+    let paper = new joint.dia.Paper({
+      el: $("#paper"),
+      width: 600,
+      height: 200,
+      model: graph,
+      gridSize: 1
+    });
+
+    let rect = new joint.shapes.basic.Rect({
+      position: { x: 100, y: 30 },
+      size: { width: 100, height: 30 },
+      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+    let rect2 = rect.clone() as joint.shapes.basic.Rect;
+    rect2.translate(300);
+
+    var link = new joint.dia.Link({
+      source: { id: rect.id },
+      target: { id: rect2.id }
+    });
+
+    graph.addCells([rect, rect2, link]);
   }
 
 }
